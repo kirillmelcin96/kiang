20900ebe4ec97e79c80e276faed7e1b1ca6c5e4d
Kirill Melcin <github3@kirillmelcin.ru>
2026-09-22 23:53:01 +0300

    feat: add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
   plugins: [vue()],
+  // Tauri build settings:
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 })
